78448a2e3919fed39ab2f50c6e3b098c0413072c
willenchou <willenchou@hotmail.com>
2019-04-19 14:16:34 +0800

    <feat>[<all>[<monitor-hystrix>]] : 熔断信息监控

--- center/consumer-hystrix-demo/src/main/java/org/willen/sc/app/AppApplication.java
+++ center/consumer-hystrix-demo/src/main/java/org/willen/sc/app/AppApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 //@ComponentScan(basePackages = {"com.dai.*"})
 public class AppApplication {
 	
---
+++ monitor/turbine/src/test/java/org/willen/sc/monitor/turbine/ConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.willen.sc.monitor.turbine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
